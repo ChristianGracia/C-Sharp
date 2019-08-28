51b37e5a76f00f430919f080775fc809f8acf46f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 15:05:32 -0400

    added program using imports to call a class

---
+++ Importing/Program.cs
@@ -0,0 +1,17 @@
+﻿using Test.math;
+
+namespace Test
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Calculator = new Calculator();
+            var result = Calculator.Add(3, 4);
+            System.Console.WriteLine(result);
+   
+     
+        }
+    }
+}
---
+++ Importing/math/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test.math
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+
+        }
+    }
+}
