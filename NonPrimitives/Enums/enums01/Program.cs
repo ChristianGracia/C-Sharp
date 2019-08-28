f88ef407dfbfcbe142204faf13da4d23191bd453
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 16:48:15 -0400

    added program with enums

---
+++ NonPrimitives/Enums/enums01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace enums01
+{
+    public enum ShippingMethod
+    {
+        Regular = 1,
+        FirstClass = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+
+          
+
+            Console.WriteLine((int) method);
+
+            //casting
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+
+            //Parsing
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof (ShippingMethod), methodName);
+
+            Console.WriteLine("\n\n" + shippingMethod);
+
+         
+        }
+    }
+}
