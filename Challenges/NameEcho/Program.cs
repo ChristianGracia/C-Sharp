fc050a2e548c08a55e524e3e5623a71fb1936f88
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 19:30:37 -0400

    added program that continously askes for a name and repeats it, program stops when nothing is inputted

---
+++ Challenges/NameEcho/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NameEcho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                Console.Write("Enter name: ");
+                var name = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(name))
+                    break;
+                else
+                    Console.WriteLine("hi " + name);
+            }
+
+            
+        }
+    }
+}
