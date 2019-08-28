42fe17588525486959fd160a6ce05607ecbd6298
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 18:36:05 -0400

    completed image sizer and added challenge folder

---
+++ Challenges/ImageSizer/imageDimensionChecker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace imageDimensionChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width: ");
+            var width = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("width is: " + width);
+
+            Console.WriteLine("Enter the Length: ");
+            var length = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("length is: " + length);
+
+            var orientation = (width > length) ? width : length;
+
+            if (orientation == width)
+            {
+                Console.WriteLine("That picture is in landscape mode");
+            }
+            else
+                Console.WriteLine("That picture is in portrait mode");
+        }
+       
+    }
+    
+}
