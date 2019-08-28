2f788d30ec1f6075c8e4c838250c826d52f4236b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 11:35:18 -0400

    added polymorphism example

---
+++ Polymorphism/Test2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.Count(4);
+            worker.Count(5);
+
+            worker.Count(4.5f);
+         
+        }
+        public class Worker
+        {
+            public Worker() {
+
+            }
+            public void Count (int number)
+            {
+                if (Total == null)
+                    Total = 0;
+                Total = Total + Convert.ToSingle(number);
+                Console.WriteLine(Total.ToString());
+            }
+
+            public void Count(float number)
+            {
+                Total = Total + number;
+                Console.WriteLine(Total.ToString());
+            }
+
+            public float Total { get; set; }
+
+        }
+    }
+}
