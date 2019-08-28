f124e9369d90c145f34f97df996c7c66d53cfedd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 15:36:05 -0400

    added program using arrays

---
+++ NonPrimitives/Arrays/arrays01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+            var flags = new bool[3];
+
+            flags[0] = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(flags[i]);
+            }
+
+            var names = new string[3] { "Andrew", "Andy", "Ders"};
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+  
+        }
+    }
+}
