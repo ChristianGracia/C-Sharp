777decc2f9d6bb342d27fc23c8aa776b47cdac19
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 17:56:31 -0400

    added completed number checking program and added the folders of the next 3 I plan on creating

---
+++ NumberValidator/NumberCheck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type any number:  "); 
+            int number = Int32.Parse(Console.ReadLine());
+
+            if (number > 10 || number < 0)
+                Console.WriteLine("invalid");
+            else
+                Console.WriteLine("valid");
+        }
+    }
+}
---
+++ NumberValidator/README.md
@@ -0,0 +1,3 @@
+# NumberChecker
+
+this program askes the user for input and responds according to their input
