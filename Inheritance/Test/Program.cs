62e6e26c7c84dc604f090229d283217affa8e4b4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 10:36:44 -0400

    added object property inheritance example

---
+++ Inheritance/Test/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var man = new Man();
+            var cart = new Cart ();
+            var dog = new Dog ();
+        }
+    }
+    public class Object
+    {
+        public Object()
+        {
+
+        }
+        public bool moves { get; set; }
+
+        public string color { get; set; }
+
+    }
+
+    // class inherits properties
+    public class Man : Object
+    {
+        public Man() {
+        }
+        public string name { get; set; }
+
+    }
+    public class Cart
+    {
+        public Cart()
+        {
+
+        }
+
+    public string brand { get; set; }
+    }
+
+    public class Dog
+    {
+        public Dog()
+        {
+
+        }
+        public string breed { get; set; }
+    }
+
+
+
+}
+
+
+
+    //public class Car
+    //{
+    //  public Car (string _color, string _size){
+
+    //this.color = _color;
+    //this.size = _size; 
+    //}
+    //public string color { get; set; }
+    //public string size { get; set; }
+    //}
+
