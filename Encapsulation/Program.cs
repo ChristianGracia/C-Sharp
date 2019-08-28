64554a28ea44fc2754a4e9e7877e49305054d0db
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 11:58:18 -0400

    added encapsulation example, readme, gitignore

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var worker = new Worker();
+            var result = worker.AddNumbers(2, 3);
+            Console.WriteLine(result.ToString());
+
+
+        }
+    }
+
+    public class Worker
+    {
+        public Worker(){
+
+        }
+
+         public int AddNumbers (int num1, int num2) {
+                var result = num1 + num2;
+                return result;
+         } 
+        
+    }
+}
