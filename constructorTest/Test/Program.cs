b7b7937fe335cbb6b006c66f36dfe2c89067d3af
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 00:02:01 -0400

    added constructor test program

---
+++ constructorTest/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("yellow", "Mini");
+            var car2 = new Car("black", "large");
+     
+            Console.WriteLine("car1 is " + car.color + "\ncar2 is "  + car2.color);
+
+        }
+    }
+    public class Car
+    {
+        public Car (string _color, string _size){
+            
+                this.color = _color;
+                this.size = _size; 
+     }
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+}
