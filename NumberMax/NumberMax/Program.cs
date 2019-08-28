9368545833e8b01c957cae8935deb028a7d71d3d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 18:26:55 -0400

    added a program I wrote that takes two numbers as input and returns the higher number

---
+++ NumberMax/NumberMax/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NumberMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+
+            int firstNumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("A = " + firstNumber);
+
+            Console.WriteLine("Enter another number: ");
+
+            int secondNumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("B = " + secondNumber);
+
+
+            
+            bool flag = false;
+
+            if (firstNumber > secondNumber)
+                flag = true;
+
+            var max = flag ? firstNumber : secondNumber;
+
+            Console.WriteLine("The Higher Number is: " + max);
+
+
+
+
+        }
+    }
+}
