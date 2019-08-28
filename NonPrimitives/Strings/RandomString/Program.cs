fcc21e115993ba5679d91611c7a056c94356fc73
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 19:56:20 -0400

    added method to get random string

---
+++ NonPrimitives/Strings/RandomString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            //Console.WriteLine(random.Next(1,10));
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+            for (var x = 0; x < passwordLength; x++)
+            {
+
+                buffer[x] = (char)('a' + random.Next(0, 26));
+            }
+
+                var password = new string(buffer);
+                Console.WriteLine(password);
+        }
+    }
+}
