3af09524dcd2c6a3aaf42abfd746b83d3fd10c15
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 21:33:05 -0400

    program that takes a series of numbers, converts them to an array, and finds the max

---
+++ Challenges/MaxInASeries/MaxInASeries/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaxInASeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var max = 0;
+            Console.WriteLine("Input numbers seperated by a comma to find max: ");
+            var answer = Console.ReadLine();
+            var counter = 1;
+
+            for (var i = 0; i < answer.Length; i++)
+            {
+                if (answer[i] == ',')
+                    counter++;
+
+            }
+            var array = answer.Split(",");
+            for (var i = 0; i < counter; i++)
+            {
+                var temp = Int32.Parse(array[i].Trim());
+                if (temp > max)
+                    max = temp;
+
+            }
+            Console.WriteLine("max is: " + max);
+        }
+    }
+}
