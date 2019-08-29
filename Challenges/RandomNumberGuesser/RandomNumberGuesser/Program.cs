7351c72036106244645a74d94cfa60546b462b15
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 20:52:20 -0400

    random number guessing program working with 4 guesses allowed

---
+++ Challenges/RandomNumberGuesser/RandomNumberGuesser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RandomNumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(1, 10);
+
+            var counter = 0;
+            while (counter < 4)
+            {
+                Console.WriteLine(randomNumber);
+                Console.WriteLine("Guess the number between 1-10");
+                var guess = Int32.Parse(Console.ReadLine());
+
+                if (guess == randomNumber)
+                { Console.WriteLine("correct!!!");
+                    break;
+                }
+
+                else
+                    counter++;
+            }
+
+        }
+    }
+}
