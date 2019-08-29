509936740fe09a2b9af61cc905d2540b8f250be3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 20:44:50 -0400

    added program that askes user to input a number and returns its factorial

---
+++ Challenges/Factorializer/Factorializer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to get the factorial of: ");
+            var number = Console.ReadLine();
+            var range = Int32.Parse(number);
+            var total = range;
+
+            Console.WriteLine("length = " + range);
+
+                for (var i = (range-1); i > 0; i--)
+                {
+                total = total * i;
+                Console.WriteLine(total);
+                }
+            Console.WriteLine(number + "! = " + total);
+            
+        }
+    }
+}
