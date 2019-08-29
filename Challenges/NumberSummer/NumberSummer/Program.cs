c417ba40cb5ec978b82533f5586a97e2d363301a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 20:15:34 -0400

    program that asks user to input numbers and sums them is functional

---
+++ Challenges/NumberSummer/NumberSummer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumberSummer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter a number to add to sum");
+                var number = Console.ReadLine();
+
+                if (number == "ok")
+                {
+                    Console.WriteLine("sum is: " + sum);
+                    break;
+                }
+
+                sum += Int32.Parse(number);
+
+                Console.WriteLine("number added is " + number + "\n type ok to stop and see sum");
+            }
+        }
+    }
+}
