ae914c9647f88b3116a8fda091c1475344ecd252
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 22:16:59 -0400

    added array method program

---
+++ NonPrimitives/Arrays/arrays02/Arrays02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Arrays02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 3, 5, 6, 7, 4 };
+
+            // IndexOf
+            var index = Array.IndexOf(numbers, 6);
+
+            // Clear
+            Array.Clear(numbers, 0, 2);
+
+            //foreach (var x in numbers)
+                //Console.WriteLine(x);
+
+            // Copy
+            int[] array2 = new int[3];
+            Array.Copy(numbers, array2, 3);
+
+          
+            //foreach(var x in array2)
+                //Console.WriteLine(x);
+
+            // Sort
+            Array.Sort(numbers);
+
+            foreach(var x in numbers)
+                Console.WriteLine(x);
+
+
+            // Reverse
+
+            Array.Reverse(numbers);
+
+            foreach(var x in numbers)
+                Console.WriteLine(x);
+        }
+    }
+}
