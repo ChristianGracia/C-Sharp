749761a0f96d1bb1cba3f8f8f21539942a4b0c10
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 23:44:11 -0400

    added a program that takes input as a string, converts to array, reverses, and converts back to string

---
+++ Challenges/ArrayReverser/ArrayReverser/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayReverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name to be reversed");
+            var name = Console.ReadLine();
+            var length = name.Length;
+            Console.WriteLine(name.Length);
+
+            var array = new char[length];
+            
+            for (var i = 0; i < length; i++)
+            {
+                array[i] = name[i];
+            }
+
+            Array.Reverse(array);
+
+            string reverse = "";
+
+            for (var x = 0; x < array.Length; x++)
+            {
+                reverse += (char) array[x];
+
+            }
+    
+            Console.WriteLine(reverse);
+        }
+    }
+}
