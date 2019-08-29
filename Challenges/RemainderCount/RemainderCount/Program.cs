4e9152d2574fc8f01052b312c8cf158f438ee528
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 20:03:23 -0400

    remainder count program complete

---
+++ Challenges/RemainderCount/RemainderCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RemainderCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var divisor = 0;
+            for (var i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                    divisor++;
+            }
+            Console.WriteLine("There are " + divisor + " divsors");
+
+        }
+    }
+}
