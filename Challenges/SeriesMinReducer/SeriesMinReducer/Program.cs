d8d98cd4f7ee9875a800e0f525771e35a9718098
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-29 01:56:19 -0400

    updates

---
+++ Challenges/SeriesMinReducer/SeriesMinReducer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SeriesMinReducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] series;
+  
+            Console.WriteLine("Input a series of numbers");
+            series = Console.ReadLine();
+           
+            foreach(var x in series)
+                Console.WriteLine(x);
+
+
+
+
+        }
+    }
+}
