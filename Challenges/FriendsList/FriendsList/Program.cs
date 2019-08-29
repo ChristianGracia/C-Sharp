a31d2c06348f54d834dcc4d7bc545f9dfc0eaeb4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 23:10:13 -0400

    added friendslist challenge problem

--- Challenges/FriendsList/FriendsList/Program.cs
+++ Challenges/FriendsList/FriendsList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FriendsList
 {
@@ -6,15 +7,48 @@ namespace FriendsList
     {
         static void Main(string[] args)
         {
+            var names = new List<string>();
+            
             while (true)
             {
                 Console.WriteLine("Enter as name: ");
                 var name = Console.ReadLine();
+                names.Add(name);
+   
+
                 Console.WriteLine("name : {0}", name);
+
 
                 if (name == "")
                     break;
             }
+
+            foreach (var x in names)
+                Console.WriteLine(x);
+
+            switch (names.Count)
+            {
+                case 7:
+                    Console.WriteLine((names[1] + ", " + names[2] + " and 1 other person like your post" ));
+                    break;
+                case 6:
+                case 5:
+                    Console.WriteLine(names[1] + " and " + names[2] + " like your post");
+                    break;
+ 
+                case 4:
+                case 3:
+                case 2:
+                    Console.WriteLine(names[1] + " liked your post");
+                    break;
+                case 1:
+                    Console.WriteLine("No one has liked it yet");
+                    break;
+                default:
+                    Console.WriteLine((names[1] + " and " + names[2] + " and " + (names.Count / 2) + " people like it!"));
+                    break;
+            }
+                
         }
     }
 }
