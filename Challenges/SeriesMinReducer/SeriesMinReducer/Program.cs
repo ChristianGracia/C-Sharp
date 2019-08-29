90d11872c362759bb84cb1626f6c58ba135c3c40
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-29 14:38:42 -0400

    program that takes a n > 5 series as an input and returns the 3 min numbers using lists and arrays

--- Challenges/SeriesMinReducer/SeriesMinReducer/Program.cs
+++ Challenges/SeriesMinReducer/SeriesMinReducer/Program.cs
@@ -7,16 +7,45 @@ namespace SeriesMinReducer
         static void Main(string[] args)
         {
             var numbers = new List<int>();
-    
-            Console.WriteLine("Enter a series of numbers to be sorted: ");
-            var answer = Console.ReadLine().Split(',');
-
-            foreach (var x in answer)
+ 
+            while (true)
             {
-                numbers.Add(Int32.Parse(x.Trim()));
-       
+                Console.WriteLine("Enter a series of at least 5 numbers to be sorted: ");
+                var answer = Console.ReadLine().Split(',');
+
+                foreach (var x in answer)
+                    numbers.Add(Int32.Parse(x.Trim()));
+         
+                if (numbers.Count > 4)
+                    break;
+                Console.WriteLine("\nInvalid List!\n");
+            }
+            var array = new int[numbers.Count];
+
+            for (var i = 0; i <numbers.Count; i++)
+            {
+                array[i] = numbers[i];
             }
 
+            Array.Sort(array);
+
+            var minArray = new int[3];
+            Array.Copy(array, minArray, 3);
+
+            Console.Write("'");
+            var counter = 0;
+            foreach (var x in minArray)
+            {
+                if (counter != 0)
+                    Console.Write(", " + x);
+                else
+                {
+                    counter++;
+                    Console.Write(x);
+                }
+            }
+            Console.Write("'");
+            Console.WriteLine(" are the mins");
         }
     }
 }
