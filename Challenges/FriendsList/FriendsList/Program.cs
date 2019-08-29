413ed14dba26f53c911965c8f9b9834ca5862625
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-28 22:44:30 -0400

    set up of program using lists

---
+++ Challenges/FriendsList/FriendsList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FriendsList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter as name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("name : {0}", name);
+
+                if (name == "")
+                    break;
+            }
+        }
+    }
+}
