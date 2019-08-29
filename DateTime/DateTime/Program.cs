41eec9449f92e8123254bdff5a440a00bb446575
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-29 14:57:57 -0400

    added program with DateTime struct

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2017, 1, 1);
+            //Console.WriteLine(dateTime);
+
+            var now = DateTime.Now;
+            //Console.WriteLine("now is " + now);
+
+            var today = DateTime.Today;
+            //Console.WriteLine("today is " + today);
+
+            var hour = now.Hour;
+            var minute = now.Minute;
+
+            // immutable
+
+            var tomorrow = now.AddDays(1);
+            //Console.WriteLine(tomorrow);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+            //Console.WriteLine(now.ToString());
+
+
+            //Console.WriteLine(hour + ":" + minute);
+        }
+    }
+}
