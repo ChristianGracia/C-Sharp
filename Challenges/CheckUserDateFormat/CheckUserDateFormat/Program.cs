4e30ab3b7a2b67aab8be45de4e609e0b07da0d65
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-30 16:17:48 -0400

    working on date format check algorithm

---
+++ Challenges/CheckUserDateFormat/CheckUserDateFormat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CheckUserDateFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intCheck;
+            var validFlag = true;
+            Console.WriteLine("Enter date between 00:00 to 23:59");
+            var dateInput = Console.ReadLine();
+
+            if (Int32.TryParse(dateInput, out intCheck) || dateInput.Length > 5 || dateInput == "") 
+                Console.WriteLine("incorrect");
+
+            validFlag = false;
+
+            if (validFlag)
+            {
+           
+                var letterArray = dateInput.Split(':');
+                var hours = letterArray[0];
+                var minutes = letterArray[1];
+
+
+            }
+        }
+    }
+}
