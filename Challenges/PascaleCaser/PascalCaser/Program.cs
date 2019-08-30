1a6ec82ebebbd847c3b989fc6b24fd5ea836d525
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-30 17:12:05 -0400

    program that converts input to pascale case is fully functional

---
+++ Challenges/PascaleCaser/PascalCaser/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PascalCaser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to be converted to Pascale Case");
+            var inputString = Console.ReadLine().ToLower();
+            var stringArray = inputString.Split(" ");
+
+            for (var i = 0; i < stringArray.Length; i++)
+            {
+              stringArray[i] =   stringArray[i].Substring(0, 1).ToUpper() + stringArray[i].Substring(1);
+            }
+            var pascaleCaseString = "";
+            foreach (var item in stringArray)
+            {
+                pascaleCaseString += item;
+            }
+            Console.WriteLine(pascaleCaseString);
+        }
+    }
+}
