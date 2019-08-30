f3175322ad9194bde32f6e78072c8b150280a582
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-30 16:57:11 -0400

    date format checker program is working

--- Challenges/CheckUserDateFormat/CheckUserDateFormat/Program.cs
+++ Challenges/CheckUserDateFormat/CheckUserDateFormat/Program.cs
@@ -5,26 +5,43 @@ namespace CheckUserDateFormat
     class Program
     {
         static void Main(string[] args)
-        {
-            int intCheck;
+        {     
             var validFlag = true;
             Console.WriteLine("Enter date between 00:00 to 23:59");
-            var dateInput = Console.ReadLine();
+            var input = Console.ReadLine();
+            var dateInput = input.Split(":");
 
-            if (Int32.TryParse(dateInput, out intCheck) || dateInput.Length > 5 || dateInput == "") 
-                Console.WriteLine("incorrect");
-
-            validFlag = false;
+            if (dateInput.Length != 2 || dateInput[1].Length != 2 || dateInput[0].Length != 2)
+                validFlag = false;
 
             if (validFlag)
             {
-           
-                var letterArray = dateInput.Split(':');
-                var hours = letterArray[0];
-                var minutes = letterArray[1];
+                for (int i = 0; i <= 1; i++)
+                {
+                    for (int j = 0; j <= 1; j++)
+                    {
+                        if (Char.IsLetter(dateInput[i][j]))
+                            validFlag = false;
+                    }
+                }
+                if (validFlag)
+                {
+                    var hours = Convert.ToInt32(dateInput[0]);
+                    var minutes = Convert.ToInt32(dateInput[1]);
 
-
+                    if (hours > 23 || hours < 0 || minutes < 0 || minutes > 59)
+                    {
+                        validFlag = false;
+                    }
+                }
             }
+            if (validFlag)
+            {
+                Console.WriteLine("correct time format");
+             
+            }
+            else
+                Console.WriteLine("incorrect time format");
         }
     }
 }
