1c9e98e144a43be98cbb9210cefe871ab83a1dec
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-31 13:07:39 -0400

    odd number remover is working

--- Challenges/OddNumberDeleter/OddNumberDeleter/Program.cs
+++ Challenges/OddNumberDeleter/OddNumberDeleter/Program.cs
@@ -6,18 +6,24 @@ namespace OddNumberDeleter
     {
         static void Main(string[] args)
         {
+
+            var oddsRemovedString = "";
             Console.WriteLine("Enter a series of numbers seperated by commas for the odd numbers to be removed");
-            var inputSeries = Console.ReadLine().Trim();
-            var inputArray = inputSeries.Split(",");
+            var inputSeries = Console.ReadLine().Split(",");
 
-            for (var i = 0; i < inputSeries.Length /3; i++)
+           for (var i = 0; i < inputSeries.Length; i++)
             {
-                Console.WriteLine(inputArray[i]);
+                inputSeries[i] = inputSeries[i].Trim();
+                if ((Convert.ToInt32(inputSeries[i]) & 1) == 0)
+                {
+                    oddsRemovedString += inputSeries[i];
+                    oddsRemovedString += ", ";
+                }
             }
-
-
-
-
+           if (oddsRemovedString.Length > 0)
+                Console.WriteLine("Series with odds removed: " + oddsRemovedString.Substring(0, oddsRemovedString.Length - 2));
+           else
+                Console.WriteLine("no even numbers");
         }
     }
 }
