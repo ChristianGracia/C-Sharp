7b160bde3ae1600b99bee0a7c6c17c3b37bdc95d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-31 12:26:07 -0400

    adding program that searches an inputted word for duplicates and lets the user know if the word contains two letters in a row or not

---
+++ Challenges/DoubleLetterChecker/DoubleLetterChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DoubleLetterChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boolFlag = false;
+            Console.WriteLine("Enter a word to search for doubles!");
+            var word = Console.ReadLine();
+            Console.WriteLine("word: " + word);
+
+            for (int i = 0; i < word.Length -1; i++)
+            {
+                if (word[i] == word[i + 1])
+                   boolFlag = !boolFlag;
+
+            }
+            if (boolFlag)
+                Console.WriteLine("Duplicates found. Word contains two of the same lettes in a row");
+            else
+                Console.WriteLine("No Duplicates letters in a row found");
+        }
+    }
+}
