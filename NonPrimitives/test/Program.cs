c523f7cea313d4c814a6d0e20faf562d63fb31af
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-31 11:13:23 -0400

    added program that takes a date as an input and calculates how many days ago it was

---
+++ NonPrimitives/test/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DateFromNow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter old date: ");
+            Console.WriteLine("seperated by hyphens ex. (12-12-2010)");
+
+            var daysAgo = 0;
+      
+            var oldDate = Console.ReadLine();
+            var oldDateArray = oldDate.Split("-");
+
+            if (oldDateArray.Length > 2)
+            {
+
+                var oldYear = Convert.ToInt32(oldDateArray[2]);
+                var oldMonth = Convert.ToInt32(oldDateArray[0]);
+                var oldDay = Convert.ToInt32(oldDateArray[1]);
+                Console.WriteLine("old date: " + oldYear + "/"+ oldMonth + "/"+ oldDay);
+
+                var currentDate = DateTime.Now.ToString();
+                var currentDateArray = currentDate.Split("/");
+
+                var year = Convert.ToInt32(currentDateArray[2].Substring(0, 4));
+                var month = Convert.ToInt32(currentDateArray[0]);
+                var day = Convert.ToInt32(currentDateArray[1]);
+
+                Console.WriteLine("year: " + year + "\nmonth: " + month + "\nday: " + day);
+
+                if (year != oldYear)
+                {
+                    var yearDifference = (year - oldYear) * 365;
+                    Console.WriteLine("day difference is " + yearDifference);
+                    daysAgo += yearDifference;
+           
+                }
+
+                if(month != oldMonth)
+                {
+                    var monthDifference = (month - oldMonth) * 30;
+                    daysAgo += monthDifference;
+                }
+                if (day != oldDay)
+                {
+                    var dayDifference = (day - oldDay);
+                    daysAgo += dayDifference;
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("incorrect usage\nPlease enter a date seperated by hypens");
+            }
+        }
+    }
+}
