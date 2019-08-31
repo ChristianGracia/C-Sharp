0e6989cb24c416184e4f4f540a17890fcb4544f2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-31 12:35:40 -0400

    added program that askes user to input a string and how many times they want each letter repeated and returns the string

---
+++ Challenges/LetterRepeater/LetterRepeater/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LetterRepeater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newString = "";
+            Console.WriteLine("Enter a string to repeat each letter");
+            var inputString = Console.ReadLine();
+
+            Console.WriteLine("Enter how many times you want each letter repeated");
+            var factor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("word: " + inputString + "\nn = " + factor);
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                for (int j = 0; j < factor; j++)
+                {
+                    newString += inputString[i];
+
+                }
+            }
+            Console.WriteLine("repeated stringn: " + newString);
+        }
+    }
+}
