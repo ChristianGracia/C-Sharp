4b0a581e7294de47a49b3f603b3eee9c82714568
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-31 12:46:40 -0400

    odd number remover program set up

---
+++ Challenges/OddNumberDeleter/OddNumberDeleter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OddNumberDeleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of numbers seperated by commas for the odd numbers to be removed");
+            var inputSeries = Console.ReadLine().Trim();
+            var inputArray = inputSeries.Split(",");
+
+            for (var i = 0; i < inputSeries.Length /3; i++)
+            {
+                Console.WriteLine(inputArray[i]);
+            }
+
+
+
+
+        }
+    }
+}
