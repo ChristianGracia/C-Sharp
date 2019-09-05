fcc8f648cff24ec342827811237917420a6d2b4b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-04 21:29:02 -0400

    added shortening string program

---
+++ NonPrimitives/Strings/strings03/string03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace string03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "really really really really really really long text im gonna shorten";
+            const int maxLength = 20;
+
+            if(sentence.Length < maxLength)
+            Console.WriteLine(sentence);
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                        break;
+                }
+                var summary = String.Join(" ", summaryWords) + "...";
+                Console.WriteLine(summary);
+
+            }
+        }
+
+    }
+}
