7a80e61c19bdc452cb385784981a7e19b19b55f7
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-05 02:22:47 -0400

    added program working with the directory class

---
+++ other/Directories/directories01/directories01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace directories01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\projects\etc");
+
+            var files = Directory.GetFiles(@"c:\projects\tmp\", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\projects\tmp\", "*.*", SearchOption.AllDirectories);
+
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+        }
+    }
+}
