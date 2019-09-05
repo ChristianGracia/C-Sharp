8653dade003248bd6bf48626630a9861d0a9c1c2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-04 21:45:05 -0400

    updated string shortening method and put it in a seperate class

--- NonPrimitives/Strings/strings03/string03/Program.cs
+++ NonPrimitives/Strings/strings03/string03/Program.cs
@@ -7,29 +7,9 @@ namespace string03
     {
         static void Main(string[] args)
         {
-            string sentence = "really really really really really really long text im gonna shorten";
-            const int maxLength = 20;
-
-            if(sentence.Length < maxLength)
-            Console.WriteLine(sentence);
-            else
-            {
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
-
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
-                    totalCharacters += word.Length + 1;
-                    if (totalCharacters > maxLength)
-                        break;
-                }
-                var summary = String.Join(" ", summaryWords) + "...";
-                Console.WriteLine(summary);
-
-            }
+            string sentence= "really really really really really really long text im gonna shorten";
+            var summary = StringShortener.SummarizeText(sentence);
+            Console.WriteLine(summary);
         }
-
     }
 }
---
+++ NonPrimitives/Strings/strings03/string03/StringShortener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace string03
+{
+    public class StringShortener
+    {
+        public static string SummarizeText(string text)
+        {
+
+            const int maxLength = 20;
+
+            if (text.Length < maxLength)
+                return text;
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+            var summary = String.Join(" ", summaryWords) + "...";
+            return summary;
+
+        }
+    }
+}
