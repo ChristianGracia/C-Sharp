44af1ed8356c0e503764c11191a95a669c969c78
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-05 02:02:06 -0400

    added program with file classes

---
+++ other/Files/files01/files01/files01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace files01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myFile.jpg", @"d:\temp\myFile.jpg",true);
+
+            File.Delete(path);
+
+            if (File.Exists(path))
+            {
+                // do something
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //use filestream for fileInfo 
+            }
+
+            // to read with file
+            File.ReadAllText();
+        }
+    }
+}
