73dcd6a2a4cc6b4c1cc577123b5997954a872ba3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-05 00:20:42 -0400

    refactored according to clean code, much more efficient

---
+++ Challenges/UniqueCounterCleanCode/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniqueCounter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<string>();
+            var uniques = UniqueReturner(numbers);
+            Console.WriteLine("uniques numbers are: " + uniques);
+        }
+
+        public static string UniqueReturner(List<string> numbers)
+        {
+            var uniqueNumbers = new StringBuilder();
+
+            do
+            {
+                Console.WriteLine("input numbers to count uniques");
+                var number = Console.ReadLine();
+                Console.WriteLine("type 'quit' to stop and sort");
+                if (number == "quit")
+                    break;
+             
+
+                if (numbers.Contains(number))
+                    numbers.Remove(number);
+                else
+                {
+                    numbers.Add(number);
+                }
+
+            } while (true);
+
+            var uniqueCounter = 0;
+            foreach (var number in numbers)
+            {
+                uniqueNumbers.Append(number + " ");
+                uniqueCounter++;
+            }
+            uniqueNumbers.Append("\nunique number total: " + uniqueCounter);
+
+            return uniqueNumbers.ToString();
+        }
+    }
+}
