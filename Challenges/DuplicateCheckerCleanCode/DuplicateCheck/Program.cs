80b2282ee58eaca4071b0cd79797f540344c4d49
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-05 01:17:07 -0400

    refactored a program that I wrote that checks a series of numbers for duplicates

---
+++ Challenges/DuplicateCheckerCleanCode/DuplicateCheck/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuplicateChecker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a series of numbers seperated by a hyphen to check duplicates");
+            var series = Console.ReadLine();
+
+            var duplicateResult = DuplicateResult(series);
+            Console.WriteLine(duplicateResult);
+        }
+
+        public static string DuplicateResult(string series)
+        {
+            var result = "no duplicates found";
+
+            if (series == "")
+            {
+                result = "exit";
+                return result;
+            }
+
+            var numberArray = series.Split("-");
+            var numberList = new List<int>();
+
+            for (var i = 0; i < numberArray.Length - 1; i++)
+            {
+                if (numberList.Contains(Convert.ToInt32(numberArray[i])))
+                    result = "duplicates found";
+
+                numberList.Add(Convert.ToInt32(numberArray[i]));
+            }
+
+
+            return result;
+        }
+    }
+}
