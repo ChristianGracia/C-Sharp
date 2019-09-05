304292f0fb68cbc3d032d5a743e38a68d2705328
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-04 22:37:56 -0400

    added program in c# using a stringbuilder class to manipulate a string

---
+++ NonPrimitives/Strings/StringBuilder/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new System.Text.StringBuilder("hi whats up");
+
+            //long chain of methods used on a StringBuilder class
+            //append/insert/replace/remove
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace("-", "+")
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            //access through index
+            Console.WriteLine("first char: " + builder[0]);
+   
+
+
+        }
+    }
+}
