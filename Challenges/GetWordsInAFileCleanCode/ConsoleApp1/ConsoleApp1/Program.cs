ea76f8c09e0a86cff666dd3b931660afcfbe87fd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-07 15:37:10 -0400

    folder removal and challenge init

---
+++ Challenges/GetWordsInAFileCleanCode/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\etc\hi1.txt";
+            var content = new FileInfo(path);
+            Console.WriteLine(content);
+
+        }
+    }
+}
