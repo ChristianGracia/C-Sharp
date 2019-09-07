a6fa40d2fa3133001ff408fbe0135ee3febe612c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-07 19:09:08 -0400

    added program with using static class modifiers

---
+++ Classes/Classes01/Classes01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Classes01
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string person2)
+        {
+            Console.WriteLine("hi {0}, This is {1}", person2, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("john");
+            person.Introduce("jake");
+            
+        }
+    }
+}
