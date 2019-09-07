5f8f53820c2048e48f334ae52083c01cadc7eeb8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-07 16:23:10 -0400

    added program that askes user to input a path and returns largest word in that file

--- Challenges/GetWordsInAFileCleanCode/ConsoleApp1/ConsoleApp1/Program.cs
+++ Challenges/GetWordsInAFileCleanCode/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,42 @@
 ﻿using System;
 using System.IO;
 
-namespace ConsoleApp1
+namespace  FindLongestWord
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            var path = @"C:\Projects\etc\hi1.txt";
-            var content = new FileInfo(path);
-            Console.WriteLine(content);
+        public static void Main(string[] args)
 
+        {
+            var path = "";
+            do
+            {
+                Console.WriteLine("Enter the path of a file to find the longest word\n");
+                path += Console.ReadLine();
+            } while (path == "");
+
+            var longestWord = LetterCounter(path);
+            Console.WriteLine(longestWord);
+        }
+
+        public static string LetterCounter(string path)
+        {
+            var fileArray = File.ReadAllText(path).Split('\n');
+
+            var longestWord = "";
+
+            foreach (var array in fileArray)
+            {
+                foreach (var word in array.Split(" "))
+                {
+                    if (word.Length > longestWord.Length)
+                        longestWord = word;
+                }
+            }
+
+            var result = "Longest word = " + longestWord;
+
+            return result;
         }
     }
 }
