b9f2746be27852fd66b3fc53fedda16b23876c57
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-09 22:45:00 -0400

    working on making a stopwatch program

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press start to start!");
+       
+            var startTime = DateTime.Now;
+
+            var startTime1 = new Stopwatch(DateTime.Now);
+
+        }
+    }
+
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; set; }
+
+        public DateTime Start(DateTime startTime)
+        {
+            return startTime;
+
+        }
+
+
+
+    }
+
+}
