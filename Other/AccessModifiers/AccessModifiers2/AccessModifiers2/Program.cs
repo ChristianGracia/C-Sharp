23445a554cb8ee2fcd7bb69fa44009387ec5232f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-11 17:02:19 -0400

    added program that uses private access modifiers for encapsulation

---
+++ Other/AccessModifiers/AccessModifiers2/AccessModifiers2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AccessModifiers2
+{
+    public class customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(ignoreOrder: true);
+
+            if (rating == 0)
+                Console.WriteLine("promoted to customer level 1");
+            else
+            {
+                Console.WriteLine("promoted to customer level 2");
+            }
+
+        }
+
+        private int CalculateRating(bool ignoreOrder)
+        {
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var person = new customer();
+            person.Promote();
+        }
+    }
+
+}
