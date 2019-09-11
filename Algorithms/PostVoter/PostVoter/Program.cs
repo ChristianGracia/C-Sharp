ae23855960240ba7bee312ca0bcca5545a02a764
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-10 23:49:31 -0400

    program using private modifiers to store up/downvotes on a page in an onbject fully functional

--- Algorithms/PostVoter/PostVoter/Program.cs
+++ Algorithms/PostVoter/PostVoter/Program.cs
@@ -3,20 +3,33 @@ using System.Dynamic;
 
 namespace PostVoter
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-
             var post = new Post("How to Cook", "some recipes", DateTime.Now );
+            post.UpVote();
+            post.UpVote();
+            Console.WriteLine(post.Votes);
+            post.DownVote();
+            Console.WriteLine(post.Votes);
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            Console.WriteLine(post.Votes);
+            post.DownVote();
+            Console.WriteLine(post.Votes);
         }
     }
 
-    class Post
+    public class Post
     {
-        private int _vote;
         public string Description;
         public string Title;
+     
+        private int _votes = 0;
+        public int Votes { get; private set; }
         public DateTime TimeCreated { get; private set; }
 
         public Post(string title, string description, DateTime timeCreated)
@@ -24,12 +37,8 @@ namespace PostVoter
             this.Title = title;
             this.Description = description;
             TimeCreated = timeCreated;
-            _vote = 0;
         }
-
- 
-
-
-
+        public void UpVote() => Votes += 1;
+        public void DownVote() => Votes -= 1;
     }
 }
