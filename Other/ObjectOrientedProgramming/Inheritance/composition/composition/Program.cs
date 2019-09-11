e00c3d1361c2c217fee5e558e74e195873311646
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-11 01:02:01 -0400

    added two classes that access another class read only

--- Other/ObjectOrientedProgramming/Inheritance/composition/composition/Program.cs
+++ Other/ObjectOrientedProgramming/Inheritance/composition/composition/Program.cs
@@ -2,6 +2,16 @@
 
 namespace composition
 {
+    public class Installer
+    {
+        
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
