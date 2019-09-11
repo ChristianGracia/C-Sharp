b9db7951c6dec5e1bf1c9d80e0378f6a1b53a093
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-11 00:47:17 -0400

    final init of program demonstrating class inheritance

--- Other/ObjectOrientedProgramming/Inheritance/inheritance02/inheritance02/Program.cs
+++ Other/ObjectOrientedProgramming/Inheritance/inheritance02/inheritance02/Program.cs
@@ -3,23 +3,15 @@
 namespace inheritance02
 {
     //: points to inherit from Presentation Object
-    public class Text : PresentationObject
-    {
-        public int FontSize { get; set; }
-        public string FontName { get; set; }
-
-        public void AddHyperlink(string url)
-        {
-            Console.WriteLine("we added a link to " + url);
-
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //inherits methods from presentation object class
+            //also inherits the members of object class
+            var text = new Text();
+            text.width = 100;
+            text.Copy();
         }
     }
 }
