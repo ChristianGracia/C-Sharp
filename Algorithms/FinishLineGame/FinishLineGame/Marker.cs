17e361bbc74c86ea7d9a4d9318feb798a21486df
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-17 19:12:30 -0400

    created new class that inherits from marker class

--- Algorithms/FinishLineGame/FinishLineGame/Marker.cs
+++ Algorithms/FinishLineGame/FinishLineGame/Marker.cs
@@ -6,18 +6,27 @@ namespace FinishLineGame
     {
         public int Position;
         public string Name;
-        public bool Stopped;
 
         public Marker(string name)
         {
             this.Position = 0;
             this.Name = name;
-            this.Stopped = false;
         }
 
         public void Move(int spaces)
         {
             this.Position += spaces;
+        }
+    }
+
+    public class FlMarker : Marker
+    {
+        public bool Stopped;
+        // base(param) refers to parent class
+        public FlMarker(string name) : base(name)
+        {
+            this.Stopped = false;
+
         }
     }
 }
