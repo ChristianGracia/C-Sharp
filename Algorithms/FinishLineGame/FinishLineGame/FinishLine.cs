0a81ee12c034fe4785753b48c7ae4448362f177e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-17 19:56:29 -0400

    constructor almost done for FinishLine class

--- Algorithms/FinishLineGame/FinishLineGame/FinishLine.cs
+++ Algorithms/FinishLineGame/FinishLineGame/FinishLine.cs
@@ -7,7 +7,7 @@ namespace FinishLineGame
         private readonly int[] SUITS = new int[] { 0, 1, 2, 3 };
         private readonly int[] VALUES = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
         private const int NUM_JOKERS = 2;
-        private readonly string[] MARKER_NAMES;
+        private readonly string[] MARKER_NAMES = new string[] {"a", "b", "c"};
         public Deck Deck;
         public Die RedDie;
         public Die BlackDie;
@@ -20,7 +20,7 @@ namespace FinishLineGame
             this.Players = players;
             this.Player1 = new Player(player1Name, this.MARKER_NAMES);
             this.Rand = new Random();
-            this.Deck = new Deck();
+            this.Deck = new Deck(this.VALUES, this.SUITS, NUM_JOKERS);
         }
     }
 }
