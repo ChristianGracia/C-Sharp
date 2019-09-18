2e6f7692e8b3eb5f9f7921a687290ef9e47b41cc
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 18:38:16 -0400

    added program to demostrate using interfaces with unit testing

---
+++ Other/unit-testing01/unit-testing01/OrderProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace unit_testing01
+{
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+
+        public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator();
+        }
+
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order has already been processed");
+
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order);
+                ShippingDate = DateTime.Today.AddDays(2);
+            }
+        }
+    }
+}
---
+++ Other/unit-testing01/unit-testing01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace unit_testing01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderProcessor = new OrderProcessor();
+            var order = new Order {DatePlaced = DateTime.now, TotalPrice = 100f};
+            orderProcessor.Process(order);
+        }
+    }
+
+    public class ShippingCalculator
+    {
+        public float CalculateShipping(OrderProcessor order)
+        {
+            if (order.TotalPrice < 30f)
+                return order.TotalPrice * 0.1f;
+            return 0;
+        }
+    }
+}
