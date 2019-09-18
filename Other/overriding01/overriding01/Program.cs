00d55c0c66ad3e8ebb3b6dcd9f54c6e00ff5fb4c
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 16:22:43 -0400

    added program I wrote using virtual/override to use a different logic in a method with the same name as the method that is inherited from a parent class

---
+++ Other/overriding01/overriding01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace overriding01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var circle = new Circle();
+           circle.Draw();
+        }
+    }
+
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("shape method");
+        }
+
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("circle method");
+        }
+
+    }
+}
