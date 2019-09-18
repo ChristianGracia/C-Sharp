824518412e49875127866de40746fc871b379138
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 17:34:44 -0400

    added a program using interfaces

---
+++ Other/Interfaces/interfaces01/interfaces01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace interfaces01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public interface ITaxCalculator
+    {
+        //interface instead of class prefix
+        //I before name
+        //no access modifiers
+        //no implementation, just declaration, no body
+        int Calculate();
+        //allow more loose coupling due to less code
+    }
+}
