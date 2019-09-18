2063f592f7a2ecf1ef6d02d63916e6e3387c44b6
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 02:16:15 -0400

    added program to play with large text files, this program is reading files on my disk and displaying it on the console

---
+++ Algorithms/ArrayConverter/ArrayConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter path of text file to be converted to array: \n");
+            var path = "C:\\projects\\etc\\lorem.txt";
+            string text = System.IO.File.ReadAllText(path);
+            Console.WriteLine(text);
+
+
+
+            //var newArray = text.Trim().Split();
+            //Console.WriteLine(newArray);
+        }
+    }
+}
