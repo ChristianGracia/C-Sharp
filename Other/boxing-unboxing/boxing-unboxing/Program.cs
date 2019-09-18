bd16ed9d5ac27d802a980432cb4a9abc7d86f036
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 00:35:20 -0400

    added program using boxing and unboxing with the global object class

---
+++ Other/boxing-unboxing/boxing-unboxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // boxing causes performance penalty because of extra object creation
+            // use type safety data containers such as <List><>
+
+            // boxing - converting value type instance to an object reference
+            // store on heap
+
+            int number = 10;
+            ;
+            object obj = number;
+
+            // or
+
+            object obj = 10;
+
+            //unboxing converts from object type back to value
+
+            object obj = 10;
+            int number = (int) obj;
+
+
+        }
+    }
+}
