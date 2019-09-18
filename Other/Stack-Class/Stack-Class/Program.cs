16e386e4e47f0f8a8007544e7b23a2861c8a269a
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 01:16:58 -0400

    added class that acts as a stack and stores and returns data in a last in first out manner

---
+++ Other/Stack-Class/Stack-Class/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+
+    public class Stack
+    {
+
+        public List<object> ItemList;
+
+        public Stack()
+        {
+            this.ItemList = new List<object>();
+
+        }
+
+        public void Push(object item)
+        {
+            this.ItemList.Add((object)item);
+
+        }
+
+        public object Pop()
+        {
+            var length = this.ItemList.Count;
+
+            if (length > 0)
+            {
+                object item = (object) this.ItemList[length - 1];
+                this.ItemList.RemoveAt(length - 1);
+                return item;
+            }
+            else
+                throw new InvalidOperationException();
+
+        }
+
+
+    }
+}
