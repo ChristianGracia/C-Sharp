123602bc3162e7eb5c026eee07022d2d4dbeab45
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 01:24:49 -0400

    refactored Stack class into its own file and moved this to algorithm directory

---
+++ Algorithms/Stack-Program-LIFO/Stack-Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stack_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push("hi");
+            stack.Push("sup");
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
--- Algorithms/Stack-Program-LIFO/Stack-Class/Stack.cs
+++ Algorithms/Stack-Program-LIFO/Stack-Class/Stack.cs
@@ -3,23 +3,6 @@ using System.Collections.Generic;
 
 namespace Stack_Class
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var stack = new Stack();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-            stack.Push("hi");
-            stack.Push("sup");
-            stack.Push(3);
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Pop());
-        }
-    }
-
     public class Stack
     {
         private readonly List<object> _itemList;
