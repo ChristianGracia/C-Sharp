eb46fc728cf3af57717452b7a64a31175b6225ac
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-17 23:01:36 -0400

    created program and and a few classes and demonstrated upcasting

---
+++ Other/ObjectOrientedProgramming/Inheritance/Casting/Casting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Casting
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public void Draw()
+        {
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Other/ObjectOrientedProgramming/Inheritance/Casting/Casting/Shape.cs
@@ -0,0 +1,18 @@
+﻿namespace Casting
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public void Draw()
+        {
+
+        }
+
+
+    }
+}
