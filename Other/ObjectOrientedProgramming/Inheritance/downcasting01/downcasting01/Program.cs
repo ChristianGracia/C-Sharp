8b653d3f46ff1394c674c95394dd008b4bab572f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-17 23:42:36 -0400

    added downcast

--- Other/ObjectOrientedProgramming/Inheritance/downcasting01/downcasting01/Program.cs
+++ Other/ObjectOrientedProgramming/Inheritance/downcasting01/downcasting01/Program.cs
@@ -7,6 +7,13 @@ namespace downcasting01
         static void Main(string[] args)
         {
             Shape shape = new Text();
+            //only get shape props and methods not Text's, so need downcast
+            Text text = (Text) shape;
+            //   ^ gets var to have class and methods of both text and shape class with downcast
+
+            
+
+
         }
     }
 }
