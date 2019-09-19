d961a402069ae04c34ccf51a8d29e38ec001cc3d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-19 00:16:17 -0400

    added program that acts as workflow engine that uses extensibility, got the first activity of work engineto display

---
+++ Programs/WorkFlowEngine/WorkFlowEngine/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace WorkFlowEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workFlow = new WorkFlow();
+            workFlow.Add(new InstallProgram());
+            workFlow.Run();
+
+        }
+    }
+
+    public class WorkFlow
+    {
+        private readonly IList<IActivities> _activities;
+
+        public WorkFlow()
+        {
+            _activities = new List<IActivities>();
+            
+        }
+        public void Add(IActivities activity)
+        {
+            _activities.Add(activity);
+        }
+
+        public void Run()
+        {
+            foreach (var activity in _activities)
+            {
+                activity.Execute();
+            }
+        }
+    }
+
+    public interface IActivities
+    {
+        void Execute();
+    }
+
+    public class InstallProgram : IActivities
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Installing program");
+        }
+    }
+}
