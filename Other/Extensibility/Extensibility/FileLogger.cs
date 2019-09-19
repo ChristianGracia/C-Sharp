75f7c74b70cc19cdaa11634fb099949799677be2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 22:29:49 -0400

    changed the behaviour without changing code by using extensbility -> creating a new class that uses the same interface

---
+++ Other/Extensibility/Extensibility/FileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace Extensibility
+{
+    public class FileLogger : Ilogger
+    {
+        private readonly string _path;
+        public FileLogger(string path)
+        {
+            _path = path;
+            
+        }
+        public void LogError(string message)
+        {
+            Log(message, "ERROR");
+
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "INFO");
+
+
+        }
+
+        public void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " message);
+            }
+
+        }
+
+    }
+}
--- Other/Extensibility/Extensibility/Program.cs
+++ Other/Extensibility/Extensibility/Program.cs
@@ -1,37 +1,18 @@
 ﻿using System;
 using System.Dynamic;
-using System.IO;
 
 namespace Extensibility
 {
-    public class FileLogger : Ilogger
-    {
-        private readonly string _path;
-        public FileLogger(string path)
-        {
-            _path = path;
-            
-        }
-        public void LogError(string message)
-        {
-            using (var streamWriter = new StreamWriter(_path, true))
-            {
-                streamWriter.WriteLine(message);
-            }
-        }
-
-        public void LogInfo(string message)
-        {
-
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
            var dbMigrator = new DbMigrator(new ConsoleLogger());
            dbMigrator.Migrate();
+
+           //first had console logging
+           //added file logging without changing any previous code
+           //with a new class that writes messages to a file instead
         }
     }
 }
