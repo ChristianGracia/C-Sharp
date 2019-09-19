40bfc0ddb22cbb2cf19cbe43612091bea4b9a8d3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 22:08:12 -0400

    program is now displaying to console times started and ended

--- Other/Extensibility/Extensibility/DbMigrator.cs
+++ Other/Extensibility/Extensibility/DbMigrator.cs
@@ -2,20 +2,6 @@
 
 namespace Extensibility
 {
-    public class ConsoleLogger : Ilogger
-    {
-        public void LogError(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
-        }
-
-        public void LogInfo(string message)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(message);
-        }
-    }
     public class DbMigrator
     {
         private readonly Ilogger _logger;
--- Other/Extensibility/Extensibility/Program.cs
+++ Other/Extensibility/Extensibility/Program.cs
@@ -2,25 +2,12 @@
 
 namespace Extensibility
 {
-    public interface Ilogger
-    {
-        void LogError(string message);
-        void LogInfo(string message);
-    }
-    public class DbMigrator
-    {
-        public void Migrate()
-        {
-            Console.WriteLine("migration started");
-            // details of migration
-            Console.WriteLine("migration finished");
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var dbMigrator = new DbMigrator(new ConsoleLogger());
+           dbMigrator.Migrate();
         }
     }
 }
