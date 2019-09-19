1be5f1828b870b1042b5d27b2238a03d3abb0edc
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-18 22:14:30 -0400

    added class that writes to file that also uses the Logger interface

--- Other/Extensibility/Extensibility/Program.cs
+++ Other/Extensibility/Extensibility/Program.cs
@@ -1,7 +1,31 @@
 ﻿using System;
+using System.Dynamic;
+using System.IO;
 
 namespace Extensibility
 {
+    public class FileLogger : Ilogger
+    {
+        private readonly string _path;
+        public FileLogger(string path)
+        {
+            _path = path;
+            
+        }
+        public void LogError(string message)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(message);
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
