f09ac3de578461b7ee173a431226e28096dbf4e7
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-22 17:28:15 -0400

    added folder for clean code examples in c#

---
+++ Other/Clean-Code/Clean-Code-Var-Names/clean-code-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CleanCodeVarNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //poor variable names
+            //mysterious names
+            //have to look at implmenetation to see what it does
+            //ex
+            //int od // overdueDays
+            //class page1 // ViewCustomerPage
+            //void button1_click // CheckAvailability_Click()
+            //meaningless names 
+            //ambigious names
+
+            //not too short, not too long
+            //meaningful
+            //reveal intention
+            //chose from problem domain
+        }
+    }
+
+    public class Car
+    {
+        public void Add()
+        {
+
+        }
+
+    }
+}
