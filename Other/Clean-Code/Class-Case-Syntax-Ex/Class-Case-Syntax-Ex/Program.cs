9e932103a150b0056323b1a57d91c430edeaad03
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-23 18:45:35 -0400

    added clean code info program

---
+++ Other/Clean-Code/Class-Case-Syntax-Ex/Class-Case-Syntax-Ex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Class_Case_Syntax_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Customer
+    {
+        private int _id;
+        public string Name { get; set; }
+
+        public void Charge(int amount)
+        {
+            var tax = 0;
+        }
+    }
+}
