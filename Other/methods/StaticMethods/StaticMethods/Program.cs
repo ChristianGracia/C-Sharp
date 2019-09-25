939e021b333226feec547c1042d18c3ee085b422
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-24 23:02:54 -0400

    added static method example I wrote

---
+++ Other/methods/StaticMethods/StaticMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //has to be used if not static in main
+            //var p = new Program();
+            //p.StaticMethod();
+        }
+
+        static void StaticMethod()
+        {
+            Console.WriteLine("this is static and doesn't need an instance of the class to be used");
+
+       
+        }
+    }
+}
