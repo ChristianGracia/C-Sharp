22137283f219d3c8b37b699de2a6cd49283be644
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-24 23:30:48 -0400

    added example I wrote of using override, going to implent parent class to array now to display

---
+++ Other/methods/Overriding03/Overriding03/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Overriding03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new People();
+            person.SayName();
+
+            var joe = new Joe();
+            joe.SayName();
+
+
+            var brett = new Brett();
+            brett.SayName();
+
+
+            var Dan = new Dan();
+            Dan.SayName();
+
+
+        }
+    }
+
+    public class People
+    {
+        public virtual void SayName()
+        {
+            Console.WriteLine("I don't have a name");
+        }
+    }
+
+    public class Joe : People
+    {
+        public string Name = "Joe";
+
+        public override void SayName()
+        {
+            Console.WriteLine("My name is " + Name);
+        } 
+    }
+
+    public class Brett : People
+    {
+        public string BrettName = "Brett";
+        public void SayName()
+        {
+            Console.WriteLine("My name is " + BrettName);
+        }
+
+    }
+
+    public class Dan : People
+    {
+        public string Name = "Dan";
+        public override void SayName()
+        {
+            Console.WriteLine("My name is " + Name);
+        }
+
+    }
+}
