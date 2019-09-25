a1e79ec2b18117ebd1fdbd4d127d2d881cd63d9d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-25 00:00:03 -0400

    added example of using abstract

---
+++ Other/ObjectOrientedProgramming/abstraction/abstract01/abstract01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace abstract01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.SayShape();
+            circle.Draw();
+            
+
+
+            
+        }
+    }
+
+    abstract  class Shapes
+    {
+        abstract public void Draw();
+        public void SayShape()
+        {
+            Console.WriteLine("hi");
+        }
+
+    }
+
+     class Circle : Shapes
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("circle");
+        }
+    }
+}
