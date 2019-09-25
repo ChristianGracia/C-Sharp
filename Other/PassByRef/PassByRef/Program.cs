ed9d61d215870998dd54ba55db7833ba93bb8325
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-24 22:53:38 -0400

    wrote out an example of using ref to pass by ref

--- Other/PassByRef/PassByRef/Program.cs
+++ Other/PassByRef/PassByRef/Program.cs
@@ -6,7 +6,34 @@ namespace PassByRef
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+
+            int x = 12;
+            int y = 12;
+
+            //pre change
+            Console.WriteLine("x:" + x);
+            Console.WriteLine("y:" + y);
+
+            //change
+            ChangeNames(ref x,ref y);
+
+            //post change
+            Console.WriteLine("x:" + x);
+            Console.WriteLine("y:" + y);
+
+            //changes by reference using ref
+
+        }
+
+        static void ChangeNames(ref int x, ref int y)
+        {
+            x = 9;
+            y = 9;
+
+
+            Console.WriteLine("x:" + x);
+            Console.WriteLine("y:" + y);
         }
     }
 }
