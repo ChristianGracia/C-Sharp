6741f70cad7913a3e581dc9dad4348d611f4e2b3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-26 00:26:33 -0400

    folder for middle waree file init

---
+++ Web-Apps/WebApp/WebApp/WebApplication1/Startup.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Text;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Services;
+
+//import static service
+
+namespace Site
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddTransient<StaticService>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+
+            app.Run(async (context) =>
+            {
+                var names = StaticService.GetNames();
+                StringBuilder builder = new StringBuilder();
+                foreach (var name in names)
+                {
+       
+                        builder.Append(name + " ");
+                    
+                }
+
+                await context.Response.WriteAsync(builder.ToString());
+            });
+
+            app.Use(async (context, next) =>
+            {
+                try
+                {
+                    await next();
+
+                }
+                catch (Exception e)
+                {
+                    await context.Response.WriteAsync("Error occured")
+                }
+            });
+
+
+
+
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
