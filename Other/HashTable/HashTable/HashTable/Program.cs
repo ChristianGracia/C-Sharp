606ae682a3814474ecead36c23adecb971086a8b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-27 00:17:50 -0400

    added to hashtable with object initializer and Add method

--- Other/HashTable/HashTable/HashTable/Program.cs
+++ Other/HashTable/HashTable/HashTable/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HashTable
+namespace HashTable01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //hashtable
-            
+            //create hashtable
+            //init with object
+            Hashtable hash = new Hashtable
+            {
+                {"a", "1"}, {"a", "1"}, {"a", "1"}, {"a", "1"},
+                {"a", "1"},
+                {"a", "1"},
+                {"a", "1"},
+                {"a", "1"},
+                {"a", "1"}
+            };
+
+            //or add items with Add method
+            hash.Add("a","1");
+            hash.Add("a", "1");
+            hash.Add("a", "1");
         }
     }
+
+ 
 }
