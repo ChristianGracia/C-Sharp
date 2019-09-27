4e4803f87118b813fc5c1ee9111a38ca3929176f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-27 00:23:20 -0400

    displaying items in hash table

--- Other/HashTable/HashTable/HashTable/Program.cs
+++ Other/HashTable/HashTable/HashTable/Program.cs
@@ -14,18 +14,29 @@ namespace HashTable01
             //init with object
             Hashtable hash = new Hashtable
             {
-                {"a", "1"}, {"a", "1"}, {"a", "1"}, {"a", "1"},
-                {"a", "1"},
-                {"a", "1"},
-                {"a", "1"},
-                {"a", "1"},
-                {"a", "1"}
+                {"dog", "1"},
+                {"snake", "2"},
+                {"panda", "3"},
+                {"pandaa", "12"},
+                {"pandaaa", "13"},
+                {"cat", "4"},
+                {"bird", "5"},
+                {"wizard", "6"},
+                {"bat", "7"},
+                {"skelly", "8"}
             };
 
             //or add items with Add method
-            hash.Add("a","1");
-            hash.Add("a", "1");
-            hash.Add("a", "1");
+            hash.Add("elf","9");
+            hash.Add("villain", "10");
+            hash.Add("bane", "11");
+
+            //display
+            //need type DictionaryEntry
+            foreach (DictionaryEntry item in hash)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
         }
     }
 
