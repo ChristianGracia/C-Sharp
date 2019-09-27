88aef8774c83357800a84b0979ecd52b2bc63f03
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-27 16:27:41 -0400

    added more SortedList methods

--- Other/SortedLists01/SortedLists01/Program.cs
+++ Other/SortedLists01/SortedLists01/Program.cs
@@ -13,14 +13,14 @@ namespace SortedLists01
             //generic collections are faster then non generic collections when using value types and more convenient when using reference types.
 
             //store key value pairs in ascending order from key.
-            //value can not be null or duplicate
+            //key can not be null or duplicate
 
             //create
             SortedList sortedList = new SortedList();
 
             //add
             sortedList.Add(1, "Jan");
-            sortedList.Add(2, "Feb");
+            sortedList.Add(2, "Jan");
             sortedList.Add(3, "March");
             sortedList.Add(4, "April");
             sortedList.Add(5, "May");
@@ -28,11 +28,37 @@ namespace SortedLists01
 
             //display
             foreach (DictionaryEntry item in sortedList)
+                Console.WriteLine(item.Key + ": " + item.Value);
+
+            //add more that will be sorted
+            Console.WriteLine("\nUpdated List\n");
+
+            //item to be added and sorted accordingly
+            sortedList.Add(0, "null");
+
+            //display list with new item to be sorted
+            foreach (DictionaryEntry item in sortedList)
+                Console.WriteLine(item.Key + ": " + item.Value);
+
+            Console.WriteLine("\nwith for loop\n");
+
+            //display values with for loop
+            for (var i = 0; i < sortedList.Count; i++)
             {
-                Console.WriteLine(item.Key + ": " + item.Value
-                                  );
+                Console.WriteLine(sortedList.GetKey(i) + ": " + sortedList.GetByIndex(i));
             }
 
+            Console.WriteLine("\nby index\n");
+            //by index
+            Console.WriteLine(sortedList[5].ToString());
+            //capacity
+            Console.WriteLine("\n" + sortedList.Capacity + "\n");
+
+            //remove
+            sortedList.Remove(1);
+
+            //check what list has by key
+            Console.WriteLine(sortedList.Contains(1));
         }
     }
 }
