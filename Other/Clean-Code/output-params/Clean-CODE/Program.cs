2faeabc90a4e258140ccdd04afdf859a71b5700d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-29 17:05:54 -0400

    added code with output params to be refactored

--- Other/Clean-Code/output-params/Clean-CODE/Program.cs
+++ Other/Clean-Code/output-params/Clean-CODE/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Clean_Code
 {
@@ -7,6 +9,27 @@ namespace Clean_Code
         static void Main(string[] args)
         {
      
+        }
+    }
+
+    public class OutputParams
+    {
+        public void DisplayCustomers()
+        {
+            int totalCount = 0;
+            var customers = GetCustomers(1, out totalCount);
+            Console.WriteLine("total customers " + totalCount);
+
+            foreach (var c in customers)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        public IEnumerable<Customer> GetCustomers(int pageIndex, out int totalCount)
+        {
+            totalCount = 100;
+            return new List<Customer>();
         }
     }
 }
