589eb66baf6624351da82ab94b06980afcf5aa2e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-29 19:09:33 -0400

    added magic numbers example

---
+++ Other/Clean-Code/magic-numbers/magic-numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace magic_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class MagicNumbers
+    {
+        public void ApproveDocument(int status)
+        {
+
+            //these numbers are "magic numbers" reader does not know what these numbers mean
+            if (status == 1)
+                Console.WriteLine("do something");
+            else if (status == 2)
+                Console.WriteLine("do something else");
+        }
+        
+    }
+}
