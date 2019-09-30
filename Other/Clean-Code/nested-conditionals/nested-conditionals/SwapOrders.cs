8070fad3ed8d9f86915aa3debc2cd76a840488bb
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-29 20:37:20 -0400

    added another example of simplifying a nested conditional

--- Other/Clean-Code/nested-conditionals/nested-conditionals/SwapOrders.cs
+++ Other/Clean-Code/nested-conditionals/nested-conditionals/SwapOrders.cs
@@ -1,10 +1,61 @@
-﻿namespace nested_conditionals
+﻿using System;
+
+namespace nested_conditionals
 {
     public class SwapOrders
     {
         public void Method()
         {
             //SWAP ORDERS/////////////////////////////////////////////////////////////////
+
+
+            //original
+            if (a)
+            {
+                if (b)
+                {
+                    isValid = true;
+
+                }
+            }
+
+            if (c)
+            {
+                if (b)
+                {
+                    isValid = true;
+                }
+            }
+
+            //simplified to 3 conditionals
+
+            if (b)
+            {
+                if (a)
+                    isValid = true;
+                if (c)
+                    isValid = true;
+            }
+
+            //then combine
+
+            if (b)
+            {
+                if (a || c)
+                    isValid = true;
+            }
+
+            //then apply combine technique again
+
+            if (b && (a || c))
+                isValid = true;
+
+            //even more simplified
+
+            isValid = (b && (a || c));
+
+            //use moderation, dont make code unreadable
+
         }
     }
 }
