84221d9555bfeabcfd63fe3c03516e270377182f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-29 21:29:26 -0400

    added a method that was simplified from 6+ conditionals using the techniques I learned

--- Other/Clean-Code/nested-conditionals/nested-conditionals/Program.cs
+++ Other/Clean-Code/nested-conditionals/nested-conditionals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace nested_conditionals
 {
@@ -7,14 +8,43 @@ namespace nested_conditionals
         static void Main(string[] args)
         {
             //see classes for nested conditionals refactor examples
+            //take conditionals and extract method
         }
     }
 
-    public class SwapOrders
+
+    //Cancel method below was simplified from 6+ conditionals to be much more readable
+
+    public class Customer
     {
-        public void Method()
+        public int LoyaltyPoints { get; set; }
+
+        public bool IsGoldCustomer()
         {
-            //SWAP ORDERS/////////////////////////////////////////////////////////////////
+            return LoyaltyPoints > 100;
+        }
+    }
+
+    public class Reservation
+    {
+        public DateTime From { get; set; }
+        public Customer Customer { get; set; }
+        public bool IsCanceled { get; set; }
+        public Reservation(Customer customer, DateTime dateTime)
+        {
+            From = dateTime;
+            Customer = customer;
+        }
+
+        private bool IsCancellationPeriodOver()
+        {
+            return (Customer.IsGoldCustomer() && LessThan(24)) ||
+                   !Customer.IsGoldCustomer() && LessThan(48);
+        }
+
+        private bool LessThan(int maxHours)
+        {
+            return (From - DateTime.Now).TotalHours < maxHours;
         }
     }
 }
