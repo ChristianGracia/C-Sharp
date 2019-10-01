f0b3bda2aa3d54f7c60ea9989937886a1c6b9e82
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-01 00:16:55 -0400

    added info on clean code with comments

---
+++ Other/Clean-Code/Comments/Comments/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //dont write comments, rewrite code
+            //dont explain what (thats obvious)
+            //explain why and how
+
+
+
+
+
+
+            //comments are usually code smells
+
+            //best comment is the code itself
+
+            //just clarify the code instead of commenting
+
+            //TODO is okay
+
+            //reduce methods functionality to single function for better name and no need for comments
+
+            //bad comment examples:
+
+
+
+
+            //comments that state the obvious such as 
+
+            //returns list of customers
+            public List<Customer> GetCustomers()
+            {
+
+            }
+
+
+            //version history is bad
+
+            //prior to v1.3
+            if (isActive) { }
+
+
+
+
+        }
+    }
+}
