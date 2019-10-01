aa3171cd4182563a5495e12c4007aa0bb26ecd70
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-30 22:56:12 -0400

    added example of duplicate code I will refactor

--- Other/Clean-Code/duplicate-code/duplicate-code/Program.cs
+++ Other/Clean-Code/duplicate-code/duplicate-code/Program.cs
@@ -7,7 +7,41 @@ namespace duplicate_code
         static void Main(string[] args)
         {
             //DRY principle - do not repeat yourself
-            //highligh repeated code, extract method with resharp
+            //highlight repeated code, extract method with resharp
+
+
+
         }
+    }
+
+    //before refactor
+    class DuplicateCode
+    {
+        public void AdmitGuest(string name, string admissionDateTime)
+        {
+            //logic
+
+            int time;
+            int hours = 0;
+            int minutes = 0;
+
+            if (!string.IsNullOrWhiteSpace(admissionDateTime))
+            {
+                if (int.TryParse(admissionDateTime.Replace(":", ""), out time))
+                {
+                    hours = time / 100;
+                    minutes = time % 100;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("AdmissionDateTime");
+            }
+        }
+    }
+
+    class DuplicateCodeRefactored
+    {
+
     }
 }
