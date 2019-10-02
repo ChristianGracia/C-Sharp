3cebef53289e65652358c6a364a4778c49e429d3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-02 19:45:00 -0400

    program demoing how to deal with long methods init and name changed of program for directory uniformity

---
+++ Other/Clean-Code/long-methods/long-methods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace long_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Single responsibility principal
+          //method should do only one thing and do it well
+        }
+    }
+}
