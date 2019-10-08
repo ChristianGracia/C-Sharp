cce3ed7b7cb2523c48b0690e420f973573ebf126
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-07 23:43:15 -0400

    added program working with linq methods

---
+++ Demos/linq/sorting-algorithm/sorting-algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace sorting_algorithm
+{
+    public class Program
+    {
+        public static string GetForm(string query)
+        {
+            return query
+                .Split(new[] { " " })
+                .Select()
+        }
+    }
+
+
+}
