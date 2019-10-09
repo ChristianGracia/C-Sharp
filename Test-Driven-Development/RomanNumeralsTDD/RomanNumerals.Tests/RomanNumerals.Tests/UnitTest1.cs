1b043b40686e9d2e61da27c8843ba5707134aec3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-09 19:57:03 -0400

    added dictionary to map numeral values to the inputted string, sum of numerals working now too

--- Test-Driven-Development/RomanNumeralsTDD/RomanNumerals.Tests/RomanNumerals.Tests/UnitTest1.cs
+++ Test-Driven-Development/RomanNumeralsTDD/RomanNumerals.Tests/RomanNumerals.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using NUnit.Framework;
 
@@ -10,6 +11,7 @@ namespace Tests
         [TestCase(1, "I")]
         [TestCase(5, "V")]
         [TestCase(10, "X")]
+        [TestCase(2, "II")]
         public void Parse(int expected, string roman)
         {
             Assert.AreEqual(expected, Roman.Parse(roman));
@@ -18,13 +20,26 @@ namespace Tests
 
     public class Roman
     {
+        private static Dictionary<char, int> _map = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'H', 1000},
+        };
         public static int Parse(string roman)
         {
-            if (roman == "X")
-                return 10;
-            if (roman == "V")
-                return 5;
-            return 1;
+            int result = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                result += _map[roman[i]];
+            }
+
+            return result;
+
         }
     }
 }
