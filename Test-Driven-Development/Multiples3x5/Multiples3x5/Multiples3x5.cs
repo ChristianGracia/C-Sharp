e99a6003c3b10472c22c0a02c8578e76570196ff
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-09 19:26:16 -0400

    logic for if number is divisible by 5 or not divisible by 3 or 5 created through TDD

--- Test-Driven-Development/Multiples3x5/Multiples3x5/Multiples3x5.cs
+++ Test-Driven-Development/Multiples3x5/Multiples3x5/Multiples3x5.cs
@@ -8,6 +8,8 @@ namespace Tests
         [TestCase("divisible by 3", 3)]
         [TestCase("divisible by 3", 6)]
         [TestCase("divisible by 5", 5)]
+        [TestCase("not divisible 3 or 5", 7)]
+        [TestCase("divisible by 5", 10)]
         public void TestMultiples(string expected, int number)
         {
             Assert.AreEqual(expected, Multiples(number));
@@ -17,7 +19,9 @@ namespace Tests
         {
             if (number % 3 == 0)
                 return "divisible by 3";
-            return "divisible by 5";
+            if(number % 5 == 0)
+                return "divisible by 5";
+            return "not divisible 3 or 5";
         }
     }
 }
