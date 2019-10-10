9414d661de6023e127b061cb81993c047d0502fd
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-09 23:47:24 -0400

    turn logic added

--- Test-Driven-Development/TicTacToeTDD/TicTacToeTDD/TicTacToe.cs
+++ Test-Driven-Development/TicTacToeTDD/TicTacToeTDD/TicTacToe.cs
@@ -34,17 +34,57 @@ namespace Tests
                 });
             }
 
+            [Test]
+            public void MoveSameSquare_ThrowsException()
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    var game = new Game();
+                    game.MakeMove(1);
+                    game.MakeMove(1);
+                });
+            }
+
     }
 
         public class Game
         {
             public int MoveCounter { get; private set; }
 
+            private readonly State[] _board = new State[9];
+
+            public Game()
+            {
+                for(int i = 0; i < _board.Length; i++)
+                {
+                    _board[i] = State.Unset;
+                }
+            }
+
             public void MakeMove(int index)
             {
                 if (index < 1 || index > 9)
                     throw new ArgumentOutOfRangeException();
+
+                if (GetState(index) != State.Unset)
+                    throw new InvalidOperationException();
+
+                _board[index - 1] = MoveCounter % 2 == 0 ? State.Cross : State.Zero;
+
                 MoveCounter++;
             }
+
+            private State GetState(int index)
+            {
+                return _board[index - 1];
+            }
+        }
+
+        public enum State
+        {
+            Cross,
+            Zero,
+            Unset
+
         }
 }
