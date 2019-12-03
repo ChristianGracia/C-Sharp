165c26340f9a08af7d7a494120fe9f95a30c8593
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-03 17:22:36 -0500

    added selenium scraper in c# with unit tests

---
+++ Scrapers/SeleniumScraper/SeleniumScraper/ScraperTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumScraper
+{
+    class ScraperTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\Users\\chris\\Downloads\\chromedriver_win32-78");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.com";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
