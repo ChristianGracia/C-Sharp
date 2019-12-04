1465a844684cc424eb6c1084a7332da176444197
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-03 22:48:39 -0500

    set up test project

---
+++ Scrapers/SeleniumScraper/SeleniumScraper/Scraper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumScraper
+{
+    public class Scraper
+    {
+        IWebDriver driver;
+        public Scraper()
+        {
+            driver = new ChromeDriver("C:\\Users\\chris\\Downloads\\chromedriver_win32-78");
+        
+
+        }
+
+        public string Scrape(string url)
+        {
+            driver.Url = url;
+
+            string title = driver.Title;
+
+
+            driver.Close();
+
+            return title;
+
+        }
+    }
+
+}
---
+++ Scrapers/SeleniumScraper/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc191df7-6eb4-4cde-9b7e-264fdd25253b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
