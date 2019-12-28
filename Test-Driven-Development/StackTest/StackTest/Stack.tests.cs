a73e3baee124637fcadeed9314fba17d59262e49
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-28 14:45:06 -0500

    added program using stack unit tests

---
+++ Test-Driven-Development/StackTest/StackTest/Stack.tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace StackTest
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void isEmpty_EmptyStack_ReturnsTrue()
+        {
+            MyStack stack = new MyStack();
+            Assert.IsTrue(stack.IsEmpty);
+
+        }
+    }
+
+    public class MyStack
+    {
+        public bool IsEmpty
+        {
+            get { return true; }
+        }
+    }
+}
