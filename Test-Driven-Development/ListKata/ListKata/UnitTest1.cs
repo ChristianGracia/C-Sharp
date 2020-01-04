8ac717bdf579784a4752c9543675fa852934128f
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-04 17:51:32 -0500

    added more methods to generic linked list in java and tests to go with it

--- Test-Driven-Development/ListKata/ListKata/UnitTest1.cs
+++ Test-Driven-Development/ListKata/ListKata/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using NUnit.Framework;
+using NUnit.Framework.Api;
 
 namespace ListKata
 {
@@ -20,9 +21,21 @@ namespace ListKata
             MyLinkedList<int> list = new MyLinkedList<int>();
             list.AddFirst(1);
 
-            Assert.AreEqual(1, list.Head.Value);
-            Assert.AreEqual(1, list.Tail.Value);
             Assert.AreSame(list.Head, list.Tail );
+        }
+
+        [Test]
+        public void AddFirstTwoElements_ListIsInCorrectState()
+        {
+            var list = new MyLinkedList<int>();
+            list.AddFirst(1);
+            list.AddFirst(2);
+
+            Assert.AreEqual(1, list.Tail.Value);
+            Assert.AreEqual(2, list.Head.Value);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreSame(list.Head.Next, list.Tail);
+
         }
     }
 
@@ -30,6 +43,7 @@ namespace ListKata
     {
         public ListNode<T> Tail { get; private set; }
         public ListNode<T> Head { get; private set; }
+        public int Count { get; set; }
 
         public void AddFirst(T value)
         {
@@ -39,7 +53,21 @@ namespace ListKata
 
         private void AddFirst(ListNode<T> node)
         {
-            Tail = Head = node;
+ 
+            //save head
+            ListNode<T> temp = Head;
+            //point head to new node
+            Head = node;
+            //insert in to list behind head
+            Head.Next = temp;
+
+            Count++;
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+
+
         }
     }
 
