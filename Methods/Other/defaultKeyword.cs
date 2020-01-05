1017bab218c7675928ee21a34631f203df660c3e
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 17:09:58 -0500

    added demo of default keyword

---
+++ Methods/Other/defaultKeyword.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DefaultKeyword {
+    class program {
+        static void Main(string[] args){
+
+            //using 7.1 or greater
+            DateTime dateVar = default;
+            long longVar = default;
+            int intVar = default;
+
+            Console.WriteLine(dateVar);
+           
+        }
+    }
+}
