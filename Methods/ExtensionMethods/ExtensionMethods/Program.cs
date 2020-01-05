1d18bd33cff5cbbf46f2225b09341f92842b5e97
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 18:51:25 -0500

    added demo of adding methods to existing types with method extension

---
+++ Methods/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine(x.IsGreater(10));
+            string text = "hi";
+            Console.WriteLine(text.IsNumber());
+        }
+    }
+
+    static class CustomExtension
+    {
+        public static bool IsGreater(this int value, int number)
+        {
+            return value > number;
+        }
+
+        public static bool IsNumber(this string text)
+        {
+            return int.TryParse(text, out int results);
+        }
+    }
+}
