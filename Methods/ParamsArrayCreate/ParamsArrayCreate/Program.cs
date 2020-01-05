dbf90052c4ae246498557d49b7a5b739325d993d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 18:15:15 -0500

    added a demo of a method that converts its params into an array using the param keyword

---
+++ Methods/ParamsArrayCreate/ParamsArrayCreate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ParamsArrayCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopThroughArray(4, 23, 66);
+
+            LoopThroughArray("chris", "joe", "jake");
+
+            LoopThroughArray(DateTime.Now, new DateTime(2020, 1, 5));
+        }
+
+        public static void LoopThroughArray<T>(params T[] items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------");
+        }
+    }
+
+}
