a8b817537c571cddafc112550422b9722d7719c6
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 16:14:52 -0500

    added c# program that writes to a file and creates a directory on running

---
+++ Methods/Other/Files/FilesWriting/FilesWriting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FilesWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\temp\Names.txt";
+            //append with true, rewrite without true overload
+            Directory.CreateDirectory(@"C:\temp\names");
+            StreamWriter sw = new StreamWriter(file, true);
+
+            sw.WriteLine("Jack");
+            sw.WriteLine("Joseph");
+            sw.Close();
+
+        }
+    }
+}
