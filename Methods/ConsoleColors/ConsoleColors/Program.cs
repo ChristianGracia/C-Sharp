54a02efc6c63658704f5b31e01179f2476758928
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 17:04:08 -0500

    added demo of changing colors of console text and background in c#

---
+++ Methods/ConsoleColors/ConsoleColors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blueMessage = "This is blue";
+            string greenMessage = "This is green";
+            string redMessage = "This is red";
+
+            Console.BackgroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(greenMessage);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(redMessage);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(blueMessage);
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
