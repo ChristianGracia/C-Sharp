7d0469e38b3237430710a12103d2b7dd082185c1
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 17:50:28 -0500

    added method with optional params demo I made

---
+++ Methods/Other/OptionalMethods/OptionalMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace OptionalMethodParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayPersonInfo("chris", 5);
+            DisplayPersonInfo("jor", 11, "football");
+        }
+
+        public static void DisplayPersonInfo(string name, int age, string club = "hockey")
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(club);
+        }
+    }
+
+}
