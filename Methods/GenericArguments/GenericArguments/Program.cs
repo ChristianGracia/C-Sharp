63dff2e9a6a387e0b45de0c025f3ed91fee6dac4
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 18:06:33 -0500

    added demo of making a method generic to accept all value types as params

---
+++ Methods/GenericArguments/GenericArguments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayInfo("Logger logged");
+            DisplayInfo(7);
+            DisplayInfo(7.888888888888);
+            DisplayInfo(DateTime.Now);
+
+            var info = (7, "Chris", 12);
+
+            DisplayInfo(info);
+        }
+
+        public static void DisplayInfo<T>(T info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
