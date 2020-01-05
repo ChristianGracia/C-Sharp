510e3b85cd5434ad7c4987d903e65d05027ca9e0
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 16:19:36 -0500

    added simple program in c# that reads files

---
+++ Methods/Other/Files/FilesReading/FilesReading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FilesReading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string file = @"C:\temp\names.txt";
+            System.IO.StreamReader sr = new System.IO.StreamReader(file);
+            Console.WriteLine(sr.ReadToEnd());
+        }
+    }
+}
