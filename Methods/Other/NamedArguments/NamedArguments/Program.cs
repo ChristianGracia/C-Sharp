d6c04240e4c34b5ca591f2bdf457e8b2fd3b6348
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 17:58:57 -0500

    added short demo that that shows using params out of order in method call without being overloaded by naming the params

---
+++ Methods/Other/NamedArguments/NamedArguments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NamedArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            DisplayPersonInfo(club:"football", age: 7, name:"jack");
+        }
+
+        public static void DisplayPersonInfo(string name, int age, string club)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(club);
+        }
+    }
+}
