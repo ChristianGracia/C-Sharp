b6a738883b35de3c44e2dc7ac09fa0b0d305af28
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 17:07:08 -0500

    added demo of seperating digits with underscore for readability in case I need later

---
+++ Methods/Other/digitSeperator.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DigitSeperator {
+    class program {
+        static void Main(string[] args){
+            //use _ for readability
+            var distanceToSun = 149_000_000;
+            Console.WriteLine(distanceToSun);
+        }
+    }
+}
