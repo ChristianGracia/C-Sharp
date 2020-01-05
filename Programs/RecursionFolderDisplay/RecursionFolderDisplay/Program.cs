2666101a7d8ee0cf4d8571f72c151adb0b6b69cc
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 17:37:15 -0500

    added program that uses recursion to display all folders on a computer

---
+++ Programs/RecursionFolderDisplay/RecursionFolderDisplay/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace RecursionFolderDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+            path = @"C:\Users\chris\Document";
+            Console.WriteLine(path);
+            DisplayFolders(path, 0);
+        }
+
+        private static void DisplayFolders(string path, int indent)
+        {
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                Console.WriteLine($"{new string(' ', indent)} {Path.GetFileName(folder)}");
+                DisplayFolders(folder, indent + 2);
+            }
+        }
+    }
+}
