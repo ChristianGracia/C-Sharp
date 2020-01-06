6aadf60f448f27107b9122e50b63590c121dc506
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-05 19:03:52 -0500

    added formmated date extension method I wrote for the DateTime type

---
+++ Methods/DateTimeTypeExtensionMethod/DateTimeTypeExtensionMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateTimeTypeExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.FormatString());
+        }
+    }
+
+    static class DateTimeExtension
+        {
+            public static string FormatString(this DateTime date)
+            {
+                string formattedString = date.DayOfWeek.ToString() + " " + date.Day.ToString() + "\n" +
+                                         date.Month.ToString() + " " + date.Year.ToString();
+
+                return formattedString;
+            }
+    }
+    
+}
