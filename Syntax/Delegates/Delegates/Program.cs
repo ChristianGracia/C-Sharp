f816cf43bac0eed2383f1b49a52fe201456b2e60
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 17:49:18 -0500

    added program using delegates to run methods by invoking the delegate

--- Syntax/Delegates/Delegates/Program.cs
+++ Syntax/Delegates/Delegates/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Delegates
 {
+    // void delegate with no argument
+
+    // void delegate with one string argument
+
+    // void delegate with one string argument and one int
+
+    // string delegate with one int argument 
+
+    public delegate void PlayerInfoDelegate();
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            PlayerInfoDelegate chris = new PlayerInfoDelegate(DisplayInformation);
+            chris.Invoke();
+
+        }
+
+        public static void DisplayInformation()
+        {
+            Console.WriteLine("Information about player : chris");
+        }
+
+        public static void DisplayInformation(string playerName)
+        {
+            Console.WriteLine("Information about player : " + playerName);
+        }
+
+        public static void DisplayInformation(string playerName, int goals)
+        {
+            Console.WriteLine("Information about player : " + playerName + " and he score " + goals);
+        }
+
+        public static string DisplayInformation(int number)
+        {
+            string playerName = string.Empty;
+            switch (number)
+            {
+                case 7: playerName = "chris"; break;
+                case 8: playerName = "dan"; break;
+                case 10: playerName = "joe"; break;
+                default: break;
+            }
+            return playerName;
         }
     }
 }
