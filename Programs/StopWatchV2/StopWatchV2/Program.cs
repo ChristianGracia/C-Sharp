a97224eec5cc45ebbc65f39d89b501e86e5198b3
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 17:16:11 -0500

    init of stopwatch program in c# that uses the built in c# timing tool

---
+++ Programs/StopWatchV2/StopWatchV2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StopWatchV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Loop(long number)
+        {
+            for (long i = 0; i < number; i++)
+            {
+
+            }
+            Console.WriteLine("Process Complete");
+        }
+    }
+}
