55080e5f20b270af0adb235dd2c8e7181ed234c6
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 17:23:58 -0500

    function run timer complete. cool program

--- Programs/TimingFunctionsStopwatch/StopWatchV2/Program.cs
+++ Programs/TimingFunctionsStopwatch/StopWatchV2/Program.cs
@@ -9,12 +9,17 @@ namespace StopWatchV2
         {
      
             Stopwatch watch = new Stopwatch();
-            Console.WriteLine("Time: 00:00:00.0000000");
+
+            long num = 900_000_000;
+            Console.WriteLine("Timing function.... n = " + num);
           
             watch.Start();
-            Loop(900_000_000);
+            Loop(num);
             watch.Stop();
-            Console.WriteLine(watch.Elapsed);
+
+            Console.WriteLine("seconds: " + watch.Elapsed.Seconds);
+            Console.WriteLine("milliseconds: " + watch.Elapsed.Milliseconds);
+            Console.WriteLine("total milliseconds: " + watch.Elapsed.TotalMilliseconds);
         }
 
         public static void Loop(long number)
