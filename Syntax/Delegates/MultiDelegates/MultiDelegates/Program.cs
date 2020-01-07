fd76d81b6c990974a7796a4731ee612e0f33f9c0
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 18:59:29 -0500

    added short demo of chainlinklinking delegates to call a bunch of different methods with one delegate

---
+++ Syntax/Delegates/MultiDelegates/MultiDelegates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace MultiDelegate
+{
+    public delegate void SayHiDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHiDelegate sayHi = null;
+
+            sayHi = new SayHiDelegate(SayHiEnglish);
+            sayHi += new SayHiDelegate(SayHiSpanish);
+            sayHi += new SayHiDelegate(SayHiJapanese);
+            sayHi += new SayHiDelegate(SayHiItalian);
+            sayHi += new SayHiDelegate(SayHiGerman); 
+            sayHi += new SayHiDelegate(SayHiArabic);
+            sayHi();
+
+        }
+
+        public static void SayHiEnglish()
+        {
+            Console.WriteLine("Hi there.");
+        }
+
+        public static void SayHiSpanish()
+        {
+            Console.WriteLine("Hola.");
+        }
+
+        public static void SayHiItalian()
+        {
+            Console.WriteLine("Ciao.");
+        }
+
+        public static void SayHiGerman()
+        {
+            Console.WriteLine("Hallo.");
+        }
+
+        public static void SayHiArabic()
+        {
+            Console.WriteLine("Marhabaan.");
+        }
+
+        public static void SayHiJapanese()
+        {
+            Console.WriteLine("Kon'nichiwa");
+        }
+    }
+}
