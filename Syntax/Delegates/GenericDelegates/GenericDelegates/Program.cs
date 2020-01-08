51957cad0db086438ce6abf4944142a53dcfed78
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 19:12:19 -0500

    added demo I made using generic arguments in a delegate to display a string, int, array, datetime, double, etc with the same method

---
+++ Syntax/Delegates/GenericDelegates/GenericDelegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GenericDelegates
+{
+    class Program
+    {
+        public delegate T DisplayInfo<T>(T value);
+        static void Main(string[] args)
+        {
+            DisplayInfo<int> newNumber = new DisplayInfo<int>(DisplayValue);
+            Console.WriteLine(newNumber(4));
+
+            DisplayInfo<string> newString = new DisplayInfo<string>(DisplayValue);
+            Console.WriteLine(newString("hi"));
+
+            DisplayInfo<int[]> newArray = new DisplayInfo<int[]>(DisplayValue);
+            Console.WriteLine(newArray(new int[3]{1, 2, 3}));
+
+            DisplayInfo<DateTime> newDate = new DisplayInfo<DateTime>(DisplayValue);
+            Console.WriteLine(newDate(DateTime.Now));
+
+            DisplayInfo<double> newDouble = new DisplayInfo<double>(DisplayValue);
+            Console.WriteLine(newDouble(100_000_000_000));
+        }
+
+        public static T DisplayValue<T>(T value)
+        {
+            Console.WriteLine("Var type: " + value.GetType().Name);
+            return value;
+
+        }
+    }
+}
