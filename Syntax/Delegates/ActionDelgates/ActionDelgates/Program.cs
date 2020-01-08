89d37d09d199e40ffc96b260558369ebced906ad
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 23:42:42 -0500

    added program using action delegate for void methods

---
+++ Syntax/Delegates/ActionDelgates/ActionDelgates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ActionDelgates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> actionOne = DisplayMessage;
+            actionOne(12);
+
+            Action actionTwo = DisplayMessage;
+            actionTwo();
+
+        }
+
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("hi from method without param");
+
+        }
+
+        public static void DisplayMessage(int number)
+        {
+            Console.WriteLine("number is " + number);
+
+        }
+    }
+}
