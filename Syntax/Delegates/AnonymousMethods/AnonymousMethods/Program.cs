24d4f2037075d5bdff5c3c0062ba4dc7648ad695
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 19:42:23 -0500

    added a program I made that shows how to use anonymous methods in c#

---
+++ Syntax/Delegates/AnonymousMethods/AnonymousMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    class Program
+    {
+        delegate void DisplayInfo();
+        delegate void DisplayInfoBasedOnNumber(int number);
+        static void Main(string[] args)
+        {
+            DisplayInfo normalDelegate = new DisplayInfo(GetInformation);
+            normalDelegate();
+            //without new delegate ctor
+            DisplayInfoBasedOnNumber oneParameterDelegate = GetInformation;
+            oneParameterDelegate(200);
+
+            Console.WriteLine("---------------------");
+
+            DisplayInfo normalDelegateAnonymous = delegate { Console.WriteLine("hi from anonymous method" ); };
+            normalDelegateAnonymous();
+
+            DisplayInfoBasedOnNumber oneParameterDelegateAnonymous = delegate (int number) { Console.WriteLine($"hi from anonymous int i = {number}"); };
+            oneParameterDelegateAnonymous(100_000);
+        }
+        public static void GetInformation()
+        {
+            Console.WriteLine("Delegate with no param");
+        }
+
+        public static void GetInformation(int x)
+        {
+            Console.WriteLine("Delegate with one param");
+
+        }
+    }
+
+
+}
