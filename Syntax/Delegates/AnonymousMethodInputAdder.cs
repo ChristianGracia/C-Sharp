6af9f41b3947dc0531d0fb613ef203955c12d8bb
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 19:53:16 -0500

    added an anonymous method I made that askes the user for input and displays the result

---
+++ Syntax/Delegates/AnonymousMethodInputAdder.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate void AddNumsDelegate(int a, int b);
+        delegate void AddNumInputDelegate();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the anonymous function integer adder");
+
+            AddNumsDelegate adder = delegate ( int a , int b) { Console.WriteLine(a + b); };
+            adder(4, 3);
+
+            AddNumInputDelegate addInput = delegate()
+            {
+                Console.WriteLine("Enter a first number");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a second number");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{a} + {b} = {a + b}");
+                Console.WriteLine(a + b);
+            };
+            addInput();
+        }
+
+    }
+}
