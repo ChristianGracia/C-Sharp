670134d353c0f7b3fad001a892abfc9bc9d86041
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 22:57:09 -0500

    added a program where I use lambdas to get an even and odd list from a list of numbers

---
+++ Syntax/Lambda/ListWithLambda/ListWithLambda/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListWithLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(33);
+            list.Add(43);
+            list.Add(0);
+            list.Add(12);
+
+            List<int> oddList = list.Where(x => x % 2 != 0).ToList();
+            List<int> evenList = list.Where(x => x % 2 == 0).ToList();
+
+
+            Console.WriteLine("Odds");
+            foreach (int number in oddList)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Even");
+            foreach (int number in evenList)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
