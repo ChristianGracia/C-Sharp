734d9285c4c9b19cd444240a9547a3924e60ca26
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-07 20:23:30 -0500

    added program I made that uses lambda expressions

--- Syntax/Delegates/LambdaExpression/LambdaExpression/Program.cs
+++ Syntax/Delegates/LambdaExpression/LambdaExpression/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LambdaExpression
 {
+    delegate int Multiply(int n);
+
+    delegate void DisplayMessage();
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            //Multiply MultiplyNumber = delegate(int n) { { return n * 3; } };
+
+            //faster version with lambda
+            Multiply MultiplyNumber = n => n * 3;
+            Console.WriteLine(MultiplyNumber(10));
+
+
+            //DisplayMessage Message = delegate { Console.WriteLine("hi"); };
+            DisplayMessage Message = () => Console.WriteLine("hi from lambda");
+            Message();
+            
         }
     }
+
 }
