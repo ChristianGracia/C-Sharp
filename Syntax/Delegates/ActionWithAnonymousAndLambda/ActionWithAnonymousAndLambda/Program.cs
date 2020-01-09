784135d25e5d69ea6cd2d6e0d2b1d709fe4776b5
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-08 23:42:21 -0500

    added program I made that demos using anonymous methods and lambdas with action delegate

---
+++ Syntax/Delegates/ActionWithAnonymousAndLambda/ActionWithAnonymousAndLambda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ActionWithAnonymousAndLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> displayInfo = SaySomething;
+            displayInfo("hi");
+            Action<string> displayInfoAnonymous = delegate(string s) { Console.WriteLine("You inputted: " + s); };
+            displayInfoAnonymous("Anonymous");
+
+            Action<string> displayInfoLambda = s => Console.WriteLine("You inputted: " + s);
+            displayInfoLambda("Lambda");
+
+        }
+
+        public static void SaySomething(string input)
+        {
+            Console.WriteLine("You inputted: " + input);
+
+        }
+    }
+}
