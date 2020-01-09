25998bffc847b776f09df7fedaa33202d6b579f7
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-08 23:21:28 -0500

    added demo I made of using a predicate delegate to run a boolean in c#

---
+++ Syntax/Delegates/PredicateDelegates/PredicateDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PredicateDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> condition = IsAdmin;
+            Console.WriteLine(condition(10));
+            Console.WriteLine(condition(1));
+
+        }
+
+        public static bool IsAdmin(int employeeNum)
+        {
+            if (employeeNum == 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
