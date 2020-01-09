40b274c1587e9dc1611b2c3efdeffa76f493c93c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-08 23:55:52 -0500

    added demo showing the use of events with delegates

---
+++ Syntax/Delegates/Events/Events/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void messageDelegate(string name);
+
+    class Program
+    {
+        public event messageDelegate messageEvent;
+
+        public Program()
+        {
+            messageEvent += new messageDelegate(SayHiEnglish);
+            messageEvent += new messageDelegate(SayHiSpanish);
+            messageEvent += new messageDelegate(SayHiItalian);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.messageEvent("chris");
+        }
+
+        public void SayHiEnglish(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+
+
+        public void SayHiSpanish(string name)
+        {
+            Console.WriteLine("Hola " + name);
+        }
+
+        public void SayHiItalian(string name)
+        {
+            Console.WriteLine("Caio " + name);
+        }
+    }
+}
