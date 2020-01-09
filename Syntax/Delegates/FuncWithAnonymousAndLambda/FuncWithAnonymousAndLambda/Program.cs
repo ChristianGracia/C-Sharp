cb00784937a33678dc268a35c2e10e3fd3ca691f
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-08 23:27:25 -0500

    program demoing using funcs with anonymous functions and lambdas init

---
+++ Syntax/Delegates/FuncWithAnonymousAndLambda/FuncWithAnonymousAndLambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FuncWithAnonymousAndLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> convertUppercaser = StringUppercaser;
+            Console.WriteLine("Using func");
+            Console.WriteLine(convertUppercaser("uppercase me"));
+        }
+
+        public static string StringUppercaser(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
