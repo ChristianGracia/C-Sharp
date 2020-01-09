01a31a73552086e20ddcb097e3a497f4db533d06
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-08 23:31:50 -0500

    program now demos a method used in a func with an anonymous function and a lambda expression

--- Syntax/Delegates/FuncWithAnonymousAndLambda/FuncWithAnonymousAndLambda/Program.cs
+++ Syntax/Delegates/FuncWithAnonymousAndLambda/FuncWithAnonymousAndLambda/Program.cs
@@ -9,6 +9,10 @@ namespace FuncWithAnonymousAndLambda
             Func<string, string> convertUppercaser = StringUppercaser;
             Console.WriteLine("Using func");
             Console.WriteLine(convertUppercaser("uppercase me"));
+
+            Func<string, string> anonymousUppercaser = delegate(string s) { return s.ToUpper(); };
+
+            Func<string, string> lambdaUppercaser = s => s.ToUpper();
         }
 
         public static string StringUppercaser(string input)
