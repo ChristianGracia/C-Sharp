d41993c75a1631691794e41d0a219ff60b36d757
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-12 03:39:50 -0500

    created custom extension to string class that capitalizes letters in a sentance in c#

---
+++ Programs/StringCapitalizeExtension/StringCapitalizeExtension/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+namespace StringCapitalizeExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "hiii whats up";
+            Console.WriteLine(text.CapitalizeLetters());
+        }
+    }
+
+    static class CustomExtension
+    {
+        public static string CapitalizeLetters(this string text)
+        {
+            string[] words = text.Split(" ");
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i][0].ToString().ToUpper() + words[i].Substring(1);
+
+            }
+
+            return string.Join(" ", words);
+        }
+    }
+}
