46b780878a307386c669536113b0baedd54d27cc
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-12 02:39:04 -0500

    added examples of expression bodied members I made

---
+++ Syntax/Other(lots-of-stuff-here)/ExpressionBodiedMembers/ExpressionBodiedMembers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace ExpressionBodiedMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddTwoNumbers(1, 4));
+            SayHi();
+            Animals dog = new Animals();
+            dog.Age = 11;
+            Console.WriteLine(dog.Age);
+        }
+
+        public static void SayHi() => Console.WriteLine("hi");
+        public static int AddTwoNumbers(int x, int y) => x + y;
+    }
+
+    class Animals
+    {
+        public Animals() => Console.WriteLine("New class created");
+        private int age;
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+    }
+}
